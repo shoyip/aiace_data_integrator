9edb989e70db5a7a88e7af215a8b343c177451eb
Shoichi Yip <shoichi.yip@unitn.it>
2022-11-09 23:22:11 +0100

    added corrections to sql queries

--- data_preparation.sql
+++ data_preparation.sql
@@ -5,21 +5,25 @@
 -- analyses of the AIACE project.
 
 -- Create conversion table between Facebook IDs and province IDs.
+DROP TABLE IF EXISTS fbid2id;
 CREATE TABLE fbid2id AS
 SELECT unnest(string_to_array(fb_province_id, '|')) fb_province_id, province_id
 FROM province_lookup ;
 
 -- Create conversion table between Facebook Names and Province IDs.
+DROP TABLE IF EXISTS fbname2id;
 CREATE TABLE fbname2id AS
 SELECT unnest(string_to_array(fb_province_name, '|')) fb_province_name, province_id
 FROM province_lookup ;
 
 -- Create conversion table between ISS Province names and Province IDs.
+DROP TABLE IF EXISTS issname2id;
 CREATE TABLE issname2id AS
 SELECT unnest(string_to_array(iss_province_name, '|')) iss_province_name, province_id
 FROM province_lookup ;
 
 -- Create movement between provinces table.
+DROP TABLE IF EXISTS movement_prov;
 CREATE TABLE movement_prov AS
 SELECT date_time,
     start_id,
@@ -33,12 +37,13 @@ FROM (
         n_crisis,
         n_baseline
     FROM movement_adm ma
-    JOIN fb2id pl1 on pl1.fb_province_id = ma.start_polygon_id::text 
-    JOIN fb2id pl2 on pl2.fb_province_id = ma.end_polygon_id::text 
+    JOIN fbid2id pl1 on pl1.fb_province_id = ma.start_polygon_id::text 
+    JOIN fbid2id pl2 on pl2.fb_province_id = ma.end_polygon_id::text 
 ) xx
 GROUP BY date_time, start_id, end_id ;
 
 -- Create Facebook population of provinces table.
+DROP TABLE IF EXISTS popluation_prov;
 CREATE TABLE population_prov AS
 SELECT date_time,
     province_id,
@@ -59,6 +64,7 @@ FROM (
 GROUP BY date_time, province_id ;
 
 -- Create COVID19 cases table.
+DROP TABLE IF EXISTS covid_prov;
 CREATE TABLE covid_prov AS
 SELECT province_id, date_time, SUM(cases)::integer cases
 FROM (
@@ -71,6 +77,7 @@ FROM (
 GROUP BY province_id, date_time ;
 
 -- Create view for total of people moving from one place.
+DROP VIEW IF EXISTS movement_prov_totalfrom;
 CREATE VIEW movement_prov_totalfrom AS
 SELECT date_time,
     start_id,
@@ -80,6 +87,7 @@ FROM movement_prov
 GROUP BY date_time, start_id ;
 
 -- Create view for people moving but every 24 hours.
+DROP VIEW IF EXISTS movement_prov_totalday;
 CREATE VIEW movement_prov_totalday AS
 SELECT datetrunc('day', date_time) date,
     start_id,
@@ -90,6 +98,7 @@ FROM movement_prov
 GROUP BY 1, 2, 3 ;
 
 -- Create view for daily probability
+DROP VIEW IF EXISTS movement_prov_totalday_totalfrom;
 CREATE VIEW movement_prov_totalday_totalfrom AS
 SELECT date,
     start_id,
@@ -99,6 +108,7 @@ FROM movement_prov_totalday
 GROUP BY 1, 2 ;
 
 -- Compute view with daily probabilities of movement.
+DROP VIEW IF EXISTS movement_daily_probabilities;
 CREATE VIEW movement_daily_probabilities AS
 SELECT date,
     start_id,
@@ -108,6 +118,7 @@ SELECT date,
 FROM movement_prov_totalday ;
 
 -- Compute probabilities along all times to move from point A to point B.
+DROP VIEW IF EXISTS movement_total_probabilities;
 CREATE VIEW movement_total_probabilities AS
 SELECT start_id,
     end_id,
@@ -123,6 +134,7 @@ FROM (
 ) xx ;
 
 -- Compute week long rolling average transfer matrix.
+DROP VIEW IF EXISTS movement_weekra_probabilities;
 CREATE VIEW movement_weekra_probabilities AS
 SELECT date,
     start_id,
